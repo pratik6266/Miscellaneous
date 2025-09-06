716194b2f963c8c698a2324ab0749aec975a5ead
Pratik Raj <pratikraj220011@gmail.com>
2025-09-06 12:10:34 +0530

    revert

--- ReactQuery/FrontEnd/src/App.tsx
+++ ReactQuery/FrontEnd/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Button } from './components/ui/button'
 import { useNavigate } from 'react-router-dom'
-import { Toast } from 'toast-with-fun'
 
 const App: React.FC = () => {
   const navigate = useNavigate()
@@ -13,7 +12,6 @@ const App: React.FC = () => {
     <>
       <div>Home Page</div>
       <Button onClick={handleOnClick}>About</Button>
-      <Button onClick={() => Toast.success('This is a success message!')}>Show Success Toast</Button>
     </>
   )
 }
